f4eeefe5166067fea8c560b7f2347cd17521c612
kapil <kapil.kumar.svp@gmail.com>
2024-10-10 18:56:40 +0000

    Made some initial changes

--- backend/main.py
+++ backend/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from fastapi.responses import JSONResponse
+from fastapi.openapi.utils import get_openapi
 
 from .routers.blog import router as blog_router
 from .routers.author import router as author_router
@@ -7,7 +8,30 @@ from .routers.category import router as category_router
 from .security.auth import router as auth_router
 
 
-app = FastAPI(title='Vblog APIs')
+app = FastAPI(title="Vblog APIs")
+
+
+# def custom_openapi():
+#     if app.openapi_schema:
+#         return app.openapi_schema
+#     openapi_schema = get_openapi(
+#         title="Vblog APIs",
+#         version="1.0.0",
+#         description="API Description",
+#         routes=app.routes,
+#     )
+#     openapi_schema["components"]["securitySchemes"] = {
+#         "BearerAuth": {
+#             "type": "http",
+#             "scheme": "bearer",
+#             "bearerFormat": "JWT",
+#         }
+#     }
+#     openapi_schema["security"] = [{"BearerAuth": []}]
+#     app.openapi_schema = openapi_schema
+#     return app.openapi_schema
+
+# app.openapi = custom_openapi
 
 @app.get('/health')
 async def health_check():
